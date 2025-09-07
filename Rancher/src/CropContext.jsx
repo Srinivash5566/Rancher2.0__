76f89fecee9d1bf480cbba1eca27812007c2bc99
Srinivash A K <srinivashak@Srinivashs-MacBook-Air.local>
2025-09-07 20:02:30 +0530

    CSS .01

--- Rancher/src/CropContext.jsx
+++ Rancher/src/CropContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import api from "./api"; // Adjust path based on your project structure
+import { sampleCrops } from "./SampleCropData__";
 
 const CropContext = createContext();
 
@@ -10,7 +11,8 @@ export const CropProvider = ({ children }) => {
     const getCrop = async () => {
       try {
         const { data } = await api.get("crop/getSeed");
-        setCropData(data || []);
+        setCropData(data.length != 0? data : sampleCrops);
+        
       } catch (err) {
         console.error("Failed to fetch crop data:", err);
       }
