552dec3c4df17c1d6cf00639e043cc249cb91394
Srinivash5566 <131421325+Srinivash5566@users.noreply.github.com>
2026-02-01 19:34:08 +0530

    Change dbUrl to a placeholder
    Updated dbUrl to a placeholder value.

--- Rancher_Node/constants/index.js
+++ Rancher_Node/constants/index.js
@@ -1,6 +1,6 @@
 const dbUrlLocal = `mongodb://localhost:27017/Rancher`;
 const dbUrl =
-  "mongodb+srv://Rancher:rancheraksh@akenterprises.1hrvj.mongodb.net/?retryWrites=true&w=majority&appName=AkEnterprises";
+  "server url";
 module.exports = {
   dbUrl,
   dbUrlLocal,
